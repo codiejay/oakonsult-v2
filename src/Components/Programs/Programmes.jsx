4c7c53b1276c9d7d7d56a855bc724a7b63c3b3f3
Ibrahim <horlalekan123@gmail.com>
2021-09-13 09:23:50 +0100

    add a function that fetch events from database and save is to an event useState

--- src/Components/Programs/Programmes.jsx
+++ src/Components/Programs/Programmes.jsx
@@ -1,62 +1,83 @@
-import { useState } from 'react';
-import './Programmes.scss';
-import ProgrammeCard from './ProgrammeCard';
-import PastProgrammes from './PastProgrammes';
-import CurrentProgrammes from './CurrentProgrammes';
+import { useEffect, useState } from "react";
+import "./Programmes.scss";
+import ProgrammeCard from "./ProgrammeCard";
+import PastProgrammes from "./PastProgrammes";
+import CurrentProgrammes from "./CurrentProgrammes";
+import { firestore } from "../../firebase/config";
 
 const Programmes = () => {
-  const [ProgrammeSort, ChangeProgrammeSort] = useState('Current');
+  const [ProgrammeSort, ChangeProgrammeSort] = useState("Current");
   let currentStyle;
   let pastStyle;
-  let colorSwitch = { currentColor: '#009ba7', pastColor: '#009ba799' };
+  let colorSwitch = { currentColor: "#009ba7", pastColor: "#009ba799" };
+  const [event, setEvent] = useState([]);
+  const eventRef = firestore.collection("events");
 
+  const OnLoadEvent = async () => {
+    eventRef.onSnapshot((snapShot) => {
+      if (!snapShot.empty) {
+        const events = [];
+
+        snapShot.docs.forEach((item, index) => {
+          events.push(item.data());
+          if (index === snapShot.size - 1) {
+            setEvent(events);
+          }
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    OnLoadEvent();
+  }, []);
   switch (ProgrammeSort) {
-    case 'Current':
-      console.log('current programmes');
-      currentStyle = { display: 'block' };
-      pastStyle = { display: 'none' };
-      colorSwitch.currentColor = '#009ba7';
-      colorSwitch.pastColor = '#009ba799';
+    case "Current":
+      console.log("current programmes");
+      currentStyle = { display: "block" };
+      pastStyle = { display: "none" };
+      colorSwitch.currentColor = "#009ba7";
+      colorSwitch.pastColor = "#009ba799";
 
       break;
-    case 'Past':
-      console.log('Past programmes');
-      currentStyle = { display: 'none' };
-      pastStyle = { display: 'block' };
-      colorSwitch.currentColor = '#009ba799';
-      colorSwitch.pastColor = '#009ba7';
+    case "Past":
+      console.log("Past programmes");
+      currentStyle = { display: "none" };
+      pastStyle = { display: "block" };
+      colorSwitch.currentColor = "#009ba799";
+      colorSwitch.pastColor = "#009ba7";
 
       break;
     default:
-      console.log('Error in programme switcher');
+      console.log("Error in programme switcher");
   }
 
   return (
-    <div id='Programmes'>
-      <div id='ProgrammesIntro'>
-        <h2 id='Title'>
+    <div id="Programmes">
+      <div id="ProgrammesIntro">
+        <h2 id="Title">
           OUR
           <br /> PROGRAMMES
         </h2>
-        <div id='calenderImage'></div>
+        <div id="calenderImage"></div>
       </div>
-      <div id='programmesSection'>
-        <div id='selectionNavBar'>
+      <div id="programmesSection">
+        <div id="selectionNavBar">
           <div
-            id='current'
-            className='programSelection'
+            id="current"
+            className="programSelection"
             onClick={() => {
-              ChangeProgrammeSort('Current');
+              ChangeProgrammeSort("Current");
             }}
             style={{ background: colorSwitch.currentColor }}
           >
             CURRENT PROGRAMS
           </div>
           <div
-            id='past'
-            className='programSelection'
+            id="past"
+            className="programSelection"
             onClick={() => {
-              ChangeProgrammeSort('Past');
+              ChangeProgrammeSort("Past");
             }}
             style={{ background: colorSwitch.pastColor }}
           >
@@ -64,12 +85,12 @@ const Programmes = () => {
           </div>
         </div>
 
-        <div id='AvailiableProgrammes'>
-          <div className='current' style={pastStyle}>
+        <div id="AvailiableProgrammes">
+          <div className="current" style={pastStyle}>
             <PastProgrammes />
           </div>
 
-          <div className='past' style={currentStyle}>
+          <div className="past" style={currentStyle}>
             <CurrentProgrammes />
           </div>
         </div>
