db312a7433f29056ecc46f48a18d26570d803894
Ibrahim <horlalekan123@gmail.com>
2021-10-24 20:27:51 +0100

    design 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Ionicons } from "react-web-vector-icons";
+import { Link } from "react-router-dom";
+import { colors } from "../../constants/Colors";
 import { Helmet } from "react-helmet";
 
 import "./styles.scss";
@@ -13,7 +16,25 @@ const NotFound = () => {
         <meta name="description" content="" />
         <meta property="og:site_name" content="OAKONSULT" />
       </Helmet>
-      <div className="notFoundContainer"></div>
+      <div className="notFoundContainer">
+        <div className="flex-center heading">
+          <Ionicons name="md-warning" size={70} color={colors.white} />
+        </div>
+        <div className="body">
+          <h3 className="404">404 Page not found</h3>
+          <span className="ooops">
+            Oopps. The page you were looking for doesn't exist.
+          </span>
+          <p className="suggestedReason">
+            You may have mistyped the address or the page might have been moved.
+          </p>
+          <div className="navigationButtons">
+            <Link to="/">
+              <button className="btn">Back to home</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
